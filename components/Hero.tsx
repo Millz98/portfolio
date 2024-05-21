602af5a22c64b70d784a6e5b0f07311a266cc1cb
Millz98 <dave_m86@hotmail.com>
2024-05-20 20:50:54 -0600

    added text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -45,7 +45,7 @@ const Hero = () => {
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless Experiences"
+            words="Transforming Concepts into Seamless User Experiences"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
